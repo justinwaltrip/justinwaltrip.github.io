e8e0f377ca69b95eba54054aa66bff0d10c8830f
Justin Waltrip <justin.waltrip@protonmail.com>
2026-02-10 12:25:03 -0500

    chore: reorganize .gitignore and add pre-commit configuration

--- devenv.nix
+++ devenv.nix
@@ -2,13 +2,19 @@
 
 {
   enterShell = ''
+    git submodule update --init --recursive
     clear
   '';
   packages = with pkgs; [
     pre-commit
     hugo
   ];
-  scripts.check.exec = ''
-    pre-commit run --all-files
-  '';
+  scripts = {
+    check.exec = ''
+      pre-commit run --all-files
+    '';
+    dev.exec = ''
+      hugo server -D
+    '';
+  };
 }
